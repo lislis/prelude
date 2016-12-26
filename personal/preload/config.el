7411f8c9b9603384d31295e4a4387d83056c1f4b
lislis <mail@lislis.de>
2016-12-26 23:52:57 +0100

    try multiple cursors

--- personal/preload/config.el
+++ personal/preload/config.el
@@ -1,2 +1,7 @@
-;; set a colo theme
+;; set a color theme
 (setq prelude-theme 'monokai)
+
+;; configure multiple cursors
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
