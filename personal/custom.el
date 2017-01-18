9ad158124eaf42bc57e6c86e8d091b21b72af2e3
lislis <mail@lislis.de>
2017-01-18 14:51:03 +0100

    set indent levels for web and js2 mode

--- personal/custom.el
+++ personal/custom.el
@@ -6,7 +6,12 @@
  '(custom-safe-themes
    (quote
     ("de30eaa469ef82373bb6f8f755c48df38c11bc461836cec9eb7c53b2007b8780" default)))
- '(js-indent-level 2))
+ '(js-indent-level 2)
+ '(js2-basic-offset 2)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-enable-auto-indentation nil)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- personal/preload/config.el
+++ personal/preload/config.el
@@ -5,3 +5,11 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  )
+(add-hook 'web-mode-hook  'my-web-mode-hook)
