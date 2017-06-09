a299978a7bb752f42526150de967f3af13c9ec8d
lislis <mail@lislis.de>
2017-06-09 22:04:09 +0200

    require monokai, comment out things

--- personal/preload/config.el
+++ personal/preload/config.el
@@ -1,10 +1,7 @@
-;; set a color theme
-(setq prelude-theme 'monokai)
-
 ;; configure multiple cursors
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+;;(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+;;(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+;;(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
